8f6a348b9db98bf5951b7f41efde69e441a94e9d
TheVideoEffect <48849405+TheVideoEffect@users.noreply.github.com>
2020-08-31 22:33:25 -0700

    Add files via upload

---
+++ Pynput.py
@@ -0,0 +1,27 @@
+from pynput.keyboard import Key, Controller
+import time
+
+keyboard =Controller()
+
+time.sleep(7)
+
+keyboard.press(Key.enter)
+keyboard.release(Key.enter)
+
+
+keyboard.type("this is a secntence written in python!")
+
+time.sleep(1)
+
+keyboard.press(Key.enter)
+keyboard.release(Key.enter)
+
+
+
+
+#keyboard.press('abcdefg')
+#keyboard.release('abcdefg')
+
+#keyboard.press(Key.cmd)
+#keyboard.relase(Key.cmd)
+
